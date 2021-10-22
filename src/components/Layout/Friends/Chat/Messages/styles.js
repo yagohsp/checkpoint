a865ddd9af552c19f2f8b864bcea2e6155ffcbab
Wandreus Vieira <wandreusvieira@gmail.com>
2021-10-22 18:00:01 -0300

    chat correction

--- src/components/Layout/Friends/Chat/Messages/styles.js
+++ src/components/Layout/Friends/Chat/Messages/styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const MessagesWrapper = styled.div`
   width: 100%;
   height: 100%;
+  overflow-y: auto;
 `;
 
 export const MessageList = styled.div`
