b6c3f34217771504f0493797512a6a89d0c8e5ac
Wandreus Vieira <wandreusvieira@gmail.com>
2021-11-06 22:02:17 -0300

    updating firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const App = firebase.initializeApp({
-  apiKey: "AIzaSyC7zfTXibAnlZEC20rWGz4Uk3-l4n2CHVo",
-  authDomain: "checkpoint-socialnetwork.firebaseapp.com",
-  projectId: "checkpoint-socialnetwork",
-  storageBucket: "checkpoint-socialnetwork.appspot.com",
-  messagingSenderId: "910853643133",
-  appId: "1:910853643133:web:5deba1e7bc35c289e74a14",
-  measurementId: "G-L25EHVKQD1"
+  apiKey: "AIzaSyBOf-V7PUpvCUSvMLxnK20ujlzESvIm5yg",
+  authDomain: "checkpoint-social-network-2021.firebaseapp.com",
+  projectId: "checkpoint-social-network-2021",
+  storageBucket: "checkpoint-social-network-2021.appspot.com",
+  messagingSenderId: "681736559329",
+  appId: "1:681736559329:web:6d92c88ac3bac7d73fb5a0",
+  measurementId: "G-TXWEZQVC77"
 });
 
 export const db = firebase.firestore();
